5322d4dd45b417806caedff00d45bd6d034bc820
Jakob-worm <jkbwrm@gmail.com>
2025-02-07 10:59:48 +0100

    Can connect to the dtabase and run

--- main.py
+++ main.py
@@ -2,7 +2,16 @@
 import psycopg2
 
 app = FastAPI()
+DATABASE_URL = "postgresql://JakobWorm:SamsonDJ!@urbanechoes-fastapi-db.postgres.database.azure.com:5432/postgres"
+conn = psycopg2.connect(DATABASE_URL)
 
 @app.get("/")
 def home():
     return {"message": "Azure FastAPI Backend is running!"}
+
+@app.get("/test-db")
+def test_db():
+    cur = conn.cursor()
+    cur.execute("SELECT version();")
+    version = cur.fetchone()
+    return {"db_version": version}
