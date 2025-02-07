ccc927a9aec7b2f17723a03cb40212dbf47c7518
Jakob-worm <jkbwrm@gmail.com>
2025-02-07 15:03:19 +0100

    Started on the flutter torutiual came to 6 add funtionalty

--- urban_echoes/test/widget_test.dart
+++ urban_echoes/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:urban_echoes/main.dart';
+import 'package:urban_echoes/main(true_Works_with_azrue).dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
