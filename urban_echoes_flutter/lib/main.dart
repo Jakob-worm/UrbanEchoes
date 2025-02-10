f4afb0342bb34aa87ec219226601b462c2340802
Jakob-worm <jkbwrm@gmail.com>
2025-02-10 14:00:17 +0100

    Added some depedencies

--- urban_echoes_flutter/lib/main.dart
+++ urban_echoes_flutter/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(MyApp());
