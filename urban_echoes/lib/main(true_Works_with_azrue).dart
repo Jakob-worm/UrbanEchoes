ede3a9a2743db92526dc3c4377f11415e66ed244
Jakob-worm <jkbwrm@gmail.com>
2025-02-10 10:45:03 +0100

    Added lib to git

---
+++ urban_echoes/lib/main(true_Works_with_azrue).dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  Future<String> fetchBackendMessage() async {
+    final response = await http.get(Uri.parse(
+        "https://urbanechoes-fastapi-backend-g5asg9hbaqfvaga9.northeurope-01.azurewebsites.net"));
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return "Error: ${response.statusCode}";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Urban Echoes")),
+        body: Center(
+          child: FutureBuilder<String>(
+            future: fetchBackendMessage(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text("Error: ${snapshot.error}");
+              } else {
+                return Text(snapshot.data ?? "No data");
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
