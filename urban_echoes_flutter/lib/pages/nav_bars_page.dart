39dd5c0601df495c25cc5105c3bfee9bc8d1397c
Jakob-worm <jkbwrm@gmail.com>
2025-02-13 11:48:49 +0100

    Removed code duplication

--- urban_echoes_flutter/lib/pages/nav_bars_page.dart
+++ urban_echoes_flutter/lib/pages/nav_bars_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:urban_echoes/state%20manegers/page_state_maneger.dart';
+import 'package:urban_echoes/wigdets/navigation_items.dart';
 
 class NavBarsPage extends StatelessWidget {
   const NavBarsPage({super.key});
@@ -12,38 +13,26 @@ class NavBarsPage extends StatelessWidget {
     return Scaffold(
       body: LayoutBuilder(
         builder: (context, constraints) {
-          return Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600,
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.camera),
-                      label: Text('Take image page'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.abc),
-                      label: Text('Backend Test'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.map),
-                      label: Text('Map'),
-                    ),
-                  ],
-                  selectedIndex: NavRailPageType.values
-                      .indexOf(pageStateManager.selectedNavRailPage),
-                  onDestinationSelected: (index) => pageStateManager
-                      .setNavRailPage(NavRailPageType.values[index]),
+          if (constraints.maxWidth < 450) {
+            return Column(
+              children: [
+                Expanded(child: pageStateManager.currentPage),
+                SafeArea(
+                  child: NavigationItems(isRail: false),
                 ),
-              ),
-              Expanded(child: pageStateManager.currentPage),
-            ],
-          );
+              ],
+            );
+          } else {
+            return Row(
+              children: [
+                SafeArea(
+                  child: NavigationItems(
+                      isRail: true, isExtended: constraints.maxWidth >= 600),
+                ),
+                Expanded(child: pageStateManager.currentPage),
+              ],
+            );
+          }
         },
       ),
     );
---
+++ urban_echoes_flutter/lib/wigdets/navigation_items.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:urban_echoes/state%20manegers/page_state_maneger.dart';
+
+// This is the main widget that will be used to display the navigation items
+// So adding a new item to the navbars should be done in here
+class NavigationItems extends StatelessWidget {
+  final bool isRail;
+  final bool isExtended;
+
+  const NavigationItems(
+      {super.key, required this.isRail, this.isExtended = false});
+
+  @override
+  Widget build(BuildContext context) {
+    var pageStateManager = Provider.of<PageStateManager>(context);
+
+    final items = [
+      NavigationRailDestination(
+        icon: Icon(Icons.home),
+        label: Text('Home'),
+      ),
+      NavigationRailDestination(
+        icon: Icon(Icons.camera),
+        label: Text('Take image page'),
+      ),
+      NavigationRailDestination(
+        icon: Icon(Icons.abc),
+        label: Text('Backend Test'),
+      ),
+      NavigationRailDestination(
+        icon: Icon(Icons.map),
+        label: Text('Map'),
+      ),
+    ];
+
+    if (isRail) {
+      return NavigationRail(
+        extended: isExtended,
+        destinations: items,
+        selectedIndex: NavRailPageType.values
+            .indexOf(pageStateManager.selectedNavRailPage),
+        onDestinationSelected: (index) {
+          pageStateManager.setNavRailPage(NavRailPageType.values[index]);
+        },
+      );
+    } else {
+      return BottomNavigationBar(
+        items: items
+            .map((item) => BottomNavigationBarItem(
+                icon: item.icon, label: item.label.toString()))
+            .toList(),
+        currentIndex: NavRailPageType.values
+            .indexOf(pageStateManager.selectedNavRailPage),
+        onTap: (index) {
+          pageStateManager.setNavRailPage(NavRailPageType.values[index]);
+        },
+      );
+    }
+  }
+}
