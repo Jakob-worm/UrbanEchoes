4883cdd211bde47958cbc1b3a0fe9e8201ed800c
Jakob-worm <jkbwrm@gmail.com>
2025-02-13 12:20:06 +0100

    size cahnge

--- urban_echoes_flutter/lib/wigdets/searchbar.dart
+++ urban_echoes_flutter/lib/wigdets/searchbar.dart
@@ -11,18 +11,23 @@ class SearchBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        controller: controller,
-        onChanged: onChanged,
-        decoration: InputDecoration(
-          hintText: 'Search...',
-          prefixIcon: Icon(Icons.search),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide.none,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width *
+            0.7, // Adjust the width as needed
+        height: 50, // Adjust the height to make it thicker
+        child: TextField(
+          controller: controller,
+          onChanged: onChanged,
+          decoration: InputDecoration(
+            hintText: 'Search...',
+            prefixIcon: Icon(Icons.search),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              borderSide: BorderSide.none,
+            ),
+            filled: true,
+            fillColor: Colors.grey[200],
           ),
-          filled: true,
-          fillColor: Colors.grey[200],
         ),
       ),
     );
