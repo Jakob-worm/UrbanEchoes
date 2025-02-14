ff34ee103d000b4553766bb5f2b2642cd2cc4a67
Jakob-worm <jkbwrm@gmail.com>
2025-02-14 12:44:13 +0100

    intro page

---
+++ urban_echoes_flutter/lib/pages/intro_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+class IntroScreen extends StatelessWidget {
+  final VoidCallback onDone;
+
+  IntroScreen({required this.onDone});
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: "Welcome",
+          body: "This is the first page of the introduction.",
+          image: Center(child: Icon(Icons.info, size: 100)),
+        ),
+        PageViewModel(
+          title: "Features",
+          body: "This is the second page of the introduction.",
+          image: Center(child: Icon(Icons.featured_play_list, size: 100)),
+        ),
+        PageViewModel(
+          title: "Get Started",
+          body: "This is the third page of the introduction.",
+          image: Center(child: Icon(Icons.start, size: 100)),
+        ),
+      ],
+      showNextButton: false,
+      done: const Text("Done"),
+      onDone: onDone,
+    );
+  }
+}
