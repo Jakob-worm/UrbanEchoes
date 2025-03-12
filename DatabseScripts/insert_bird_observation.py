d46f9f4745508c9b0d9671f0584fbf73122267cd
Jakob-worm <jkbwrm@gmail.com>
2025-03-12 10:24:10 +0100

    small fix

--- DatabseScripts/insert_bird_observation.py
+++ DatabseScripts/insert_bird_observation.py
@@ -47,15 +47,15 @@ if __name__ == "__main__":
 
      # Example list of bird observations
     bird_observations = [
-        ("Sol sort", "Turdus merula", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/turdus_merula/", 56.171523, 10.189463, "2025-12-12", "12:30:00", 0, 2, False, None),
-        ("Blåmejse", "Cyanistes caeruleus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/cyanistes_caeruleus/", 56.171196, 10.190269, "2025-12-12", "12:35:00", 0, 1, False, None),
-        ("Blåmejse", "Cyanistes caeruleus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/cyanistes_caeruleus/", 56.172975, 10.193424, "2025-12-12", "12:37:00", 0, 1, False, None),
-        ("RingDue", "Columba palumbus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/columba_palumbus/", 56.173651, 10.193487, "2025-12-12", "12:38:00", 0, 3, False, None),
-        ("Råge", "Corvus frugilegus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/corvus_frugilegus/", 56.173651, 10.193487, "2025-12-12", "12:38:00", 0, 2, False, None),
-        ("Sol sort", "Turdus merula", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/turdus_merula/",56.174019, 10.191638, "2025-12-12", "12:40:00", 0, 3, False, None),
-        ("Grønirisk", "Chloris chloris", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/chloris_chloris/", 56.173262, 10.191670, "2025-12-12", "12:41:00", 0, 1, False, None),
-        ("Sølv Måge", "Larus argentatus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/larus_argentatus/", 56.173317, 10.191036, "2025-12-12", "12:42:00", 0, 1, False, None),
-        ("Skovspurv", "Passer montanus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/passer_montanus/", 56.173395, 10.194014, "2025-12-12", "12:43:00", 0, 1, False, None),
+        ("Solsort", "Turdus merula", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/turdus_merula", 56.171523, 10.189463, "2025-12-12", "12:30:00", 0, 2, False, None),
+        ("Blåmejse", "Cyanistes caeruleus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/cyanistes_caeruleus", 56.171196, 10.190269, "2025-12-12", "12:35:00", 0, 1, False, None),
+        ("Blåmejse", "Cyanistes caeruleus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/cyanistes_caeruleus", 56.172975, 10.193424, "2025-12-12", "12:37:00", 0, 1, False, None),
+        ("RingDue", "Columba palumbus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/columba_palumbus", 56.173651, 10.193487, "2025-12-12", "12:38:00", 0, 3, False, None),
+        ("Råge", "Corvus frugilegus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/corvus_frugilegus", 56.173651, 10.193487, "2025-12-12", "12:38:00", 0, 2, False, None),
+        ("Solsort", "Turdus merula", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/turdus_merula",56.174019, 10.191638, "2025-12-12", "12:40:00", 0, 3, False, None),
+        ("Grønirisk", "Chloris chloris", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/chloris_chloris", 56.173262, 10.191670, "2025-12-12", "12:41:00", 0, 1, False, None),
+        ("Sølv Måge", "Larus argentatus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/larus_argentatus", 56.173317, 10.191036, "2025-12-12", "12:42:00", 0, 1, False, None),
+        ("Skovspurv", "Passer montanus", "https://urbanechostorage.blob.core.windows.net/bird-sounds-test/passer_montanus", 56.173395, 10.194014, "2025-12-12", "12:43:00", 0, 1, False, None),
     ]
 
     insert_multiple_bird_observations(db, bird_observations)
